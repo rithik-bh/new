93ce289b162be53c76c72bff2a67c9f9092b6c32
rithik-bh <30982990+rithik-bh@users.noreply.github.com>
2019-10-02 01:09:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = 'This is a secret!'
+
+
+@app.route("/")
+def index():
+    return 'deployed'
+
+
+if __name__ == "__main__":
+    app.run()
